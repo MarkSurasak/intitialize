d3899a22013eef63c939b141c9fa08ccc2dfbd8b
MarkSurasak <61034796+MarkSurasak@users.noreply.github.com>
2022-06-17 15:23:49 +0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import NavBar from "./component/NavBar.svelte";
+</script>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Kanit:ital,wght@1,300&display=swap");
+
+  :root {
+    font-family: "Kanit", sans-serif;
+  }
+</style>
+
+<main>
+	<NavBar/>
+</main>
---
+++ component/NavBar.svelte
@@ -0,0 +1,37 @@
+<main>
+	<nav>
+		<p>My Portfolio</p>
+		<ul>
+			<a href="#">Introduction</a>
+			<a href="#">Education</a>
+			<a href="#">Performance</a>
+			<a href="#">Activition</a>
+			<a href="#">Certificate</a>
+		</ul>
+	</nav>
+</main>
+
+<style>
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  p,
+  a {
+    font-size: 18px;
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    transition: 0.5s;
+  }
+
+  a:hover {
+    color: #1b2430;
+  }
+</style>
